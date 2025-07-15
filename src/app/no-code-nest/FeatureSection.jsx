252b05663e3334912073db874a975622a055b51e
Usaid Sharique <mohdu3106@gmail.com>
2025-07-16 00:15:49 +0530

    update

--- src/app/no-code-nest/FeatureSection.jsx
+++ src/app/no-code-nest/FeatureSection.jsx
@@ -1,15 +1,58 @@
+'use client'
+
 import React from 'react'
 import { featureItems } from '@/utils/constants';
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger)
 
 const FeatureSection = () => {
+  const boxRef = useRef()
+  const cardsRef = useRef([])
+
+  useGSAP(()=>{
+  gsap.from(boxRef.current,{
+  y:60,
+  duration:1,
+  ease:"power2.out",
+  opacity:0,
+  scrollTrigger: {
+          trigger: boxRef.current,
+          start: 'top 70%',
+          end:"top 20%",
+          // markers:true,
+          toggleActions: 'play none none none',
+        },
+})
+
+gsap.from(cardsRef.current,{
+  scale:0,
+  opacity:0,
+  duration:1,
+  ease:"power2.inOut",
+  stagger:{amount:0.6},
+  scrollTrigger:{
+          trigger: boxRef.current,
+          start:"top 40%",
+           toggleActions: "play none none none", 
+          // markers:true,
+  }
+})
+
+  },[])
+
+  
   return (
-    <div className="grid grid-cols-3 lg:[grid-template-columns:minmax(0,1fr)_minmax(0,1.5fr)_minmax(0,1.5fr)_minmax(0,1fr)] gap-5 py-5 px-5 border rounded-3xl bg-black text-white relative">
+    <div ref={boxRef} className="grid grid-cols-3 lg:[grid-template-columns:minmax(0,1fr)_minmax(0,1.5fr)_minmax(0,1.5fr)_minmax(0,1fr)] gap-5 py-5 px-5 border rounded-3xl bg-black text-white relative">
       {featureItems.map((item, index) => {
         if (item.type === 'card') {
           return (
              <div
+             ref={(el) => cardsRef.current.push(el)}
               key={index}
-              className="group p-4 rounded-xl border flex flex-col gap-8 items-center justify-between text-center lg:text-start hover:scale-105 duration-200 transition-all relative hover:shadow-[0_6px_12px_0_rgba(16,185,129,1)]"
+              className="group p-4 rounded-xl border flex flex-col gap-8 items-center justify-between text-center lg:text-start hover:scale-105  relative hover:shadow-[0_6px_12px_0_rgba(16,185,129,1)]"
             >
               <div className="relative h-14 flex items-center justify-center w-full text-balance text-xs lg:text-base">
                 <span className='absolute left-0 right-0 top-0 transition-all duration-300 opacity-100 group-hover:opacity-0'>{item.title}</span>
@@ -21,7 +64,7 @@ const FeatureSection = () => {
         }
         if (item.type === 'text') {
           return (
-            <>
+            
             <div
               key={index}
               className={`${index === 6 && "hidden lg:flex"} ${index === 4 && "lg:hidden"} flex items-center justify-center text-xs font-bold
@@ -32,7 +75,7 @@ const FeatureSection = () => {
                 {item.text}
               </h1>
             </div>
-            </>
+            
           );
         }
         return null;
@@ -42,3 +85,4 @@ const FeatureSection = () => {
 }
 
 export default FeatureSection
+
--- src/app/no-code-nest/OneMinute.jsx
+++ src/app/no-code-nest/OneMinute.jsx
@@ -1,12 +1,44 @@
+'use client'
+
 import PhotoLoop from '@/components/ui/PhotoLoop'
-import React from 'react'
+import React, { useRef } from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import ScrollTrigger from 'gsap/ScrollTrigger'
 
 import { oneMinuteItems } from '@/utils/constants'
 
+gsap.registerPlugin(ScrollTrigger)
+
 const OneMinute = () => {
-    
+    const boxRef = useRef(null)
+
+    useGSAP(() => {
+gsap.set(boxRef.current,{
+autoAlpha:0,
+        y: 60,
+})
+
+      gsap.to(boxRef.current, {
+        autoAlpha:1,
+        yPercent:-20,
+        duration: 1,
+        ease: 'power2.out',
+        scrollTrigger: {
+          trigger: boxRef.current,
+          start: 'top 70%',
+          end:"top 20%",
+          // markers:true,
+          toggleActions: 'play none none reverse',
+        },
+      });
+    }, [])
+
   return (
-         <PhotoLoop className={"border-2 border-gray-300"} photos={oneMinuteItems} bgColor={"#D3FFB9"}/>
+    <div ref={boxRef} className='opacity-0'>
+      <PhotoLoop className={"border-2 border-gray-300"} photos={oneMinuteItems} bgColor={"#D3FFB9"}/>
+    </div>
+         
   )
 }
 
