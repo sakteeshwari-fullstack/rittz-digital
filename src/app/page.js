7fbe9a1fcb4cb4fafbbffab6ce222014edb817fa
AkilanRittz <akilan@rittzdigital.com>
2025-07-24 18:01:27 +0530

    google site verification

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,9 @@ import Main from "@/components/main";
 export const metadata = {
   title: "Best E-commerce Website Development Company in Chennai",
   description: "Looking for the best e-commerce website development company in Chennai? Get scalable, secure, and fully custom online store solutions tailored to your brand.",
+  other: {
+      'google-site-verification': 'f8AqA8m1bfxvtF2Y7Vn-InzDaPBidwBSLQScWEC9gVM'
+    },
 };
 
 export default function  Home  (){
